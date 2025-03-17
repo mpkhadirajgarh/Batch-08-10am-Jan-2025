8318713039471bbc9b453dc926befe67888bc9f0
mpkhadirajgarh <mpkhadirajgarh@gmail.com>
2025-03-17 10:08:02 +0530

    done

---
+++ java script/intro.js
@@ -0,0 +1,13 @@
+console.log("Welcome to Java Script");
+
+// veriable keyword  Var , Let , const
+let a = 10 ; // Number
+let city = "Sarangpur" ;  // String type data type 
+let b = 20 ; 
+
+var c =  a + b ; 
+
+
+
+console.log (c);
+console.log(city);
