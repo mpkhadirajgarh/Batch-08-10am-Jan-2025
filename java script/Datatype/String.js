2dc6992412100a3f00903193c183104f417aedae
DINESH BAMNIA <mpkhadirajgarh@gmail.com>
2025-03-18 10:13:18 +0530

    done

---
+++ java script/Datatype/String.js
@@ -0,0 +1,22 @@
+const a = 50 ;
+ const b = 60;
+
+// a = 120; const veriable ki value ko dobara declare nhi kiya ja sakta.
+
+ const c = a+ b ;
+
+ console.log(c);
+
+ // data type
+ 
+ // String data type 
+
+ let Center = "Aditya Computer"; // string 
+
+ let CentrCode = "6322"   // String 
+
+ console.log(Center, "ka Data Type :", typeof Center);
+
+ console.log(`center code: ${CentrCode} ka data type :`, typeof CentrCode);
+
+
