7770c6d8ea81a300dfe36eac0c72cab1e778587d
DINESH BAMNIA <mpkhadirajgarh@gmail.com>
2025-03-20 10:22:09 +0530

    Add files via upload

---
+++ java script/Datatype/number.js
@@ -0,0 +1,49 @@
+let a = 10 ; 
+
+// console.log(a);
+
+let b = "10";
+
+console.log(typeof b);
+
+const c = parseInt(b); // string to number conversion
+
+console.log (typeof c)
+
+// maths operation 
+
+// sum operation 
+
+const sum = a  + c ; 
+
+console.log(`Sum of a + c: `,sum);
+
+// subtraction operation 
+
+console.log(`Subtraction of a - c: `,(a-c));
+
+// multiplication operation 
+
+console.log(`Multiplication of a x c: `, (a*c));
+
+// Division / divide operation 
+
+console.log(`Division of a/c:  `,(a/c));
+
+// Remainder in java script || Modulus (Remainder)
+
+console.log(`Remainder of: `,(a % c ));
+
+console.log(19 % 3);
+
+// Exponentiation 
+
+console.log(10**3); // ghat lagane ke liyhe 
+
+// Increament and Decreament operator 
+
+
+
+
+
+
