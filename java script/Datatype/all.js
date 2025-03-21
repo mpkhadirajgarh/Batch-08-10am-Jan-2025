161a6764e01f24c28f0e5c80ce30488c10891da7
DINESH BAMNIA <mpkhadirajgarh@gmail.com>
2025-03-21 10:11:20 +0530

    Add files via upload

---
+++ java script/Datatype/all.js
@@ -0,0 +1,49 @@
+// Data type Shortcut "NNSSBBU"
+
+// Number 
+// Null 
+// String 
+// Symbol 
+// BigInt 
+// Booleaan 
+// Undefined 
+
+// Hitesh Choudary - chaicode // Piyush Garg 
+
+let a = null ; 
+
+console.log(typeof a); // Typeof = Object 
+
+
+
+let b = Symbol("city"); // Symbol 
+
+console.log(typeof b);
+
+// big Int 
+
+let c = BigInt(15545); 
+
+console.log(typeof c);
+
+// Boolean = true / False 
+
+let d = true;
+
+console.log(typeof d);
+
+//  Undefined 
+
+let e;
+
+console.log(typeof e); // undefined 
+
+
+
+
+
+
+
+
+
+
