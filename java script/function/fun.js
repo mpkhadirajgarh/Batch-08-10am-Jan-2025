7568cedbf28c6d3433d2803f04ae9ca856167949
DINESH BAMNIA <mpkhadirajgarh@gmail.com>
2025-03-26 10:12:12 +0530

    done

---
+++ java script/function/fun.js
@@ -0,0 +1,33 @@
+// 1 to 10 
+
+// for loop 
+
+// n ek parameter 
+
+function MyNumber(n)  {
+
+    for(let i = 1 ; i<=n; i++){
+        console.log(i)
+    }
+}
+
+// MyNumber(100); // 100 ek argument
+
+
+
+function MyCalc(value1 , value2){
+
+console.log(`Sum of:`,value1 + value2);
+console.log(`Subtraction of:`,value1 - value2);
+console.log(`Multiply of:`,value1 * value2);
+console.log(`Divide of:`,value1 / value2);
+console.log(`Remainder of:`,value1 % value2);
+
+}
+
+MyCalc(501,10); // function call krna.
+
+// Loop Statement in js 
+// for 
+// while
+// do while 
