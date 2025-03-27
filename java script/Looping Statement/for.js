3e2d10f5f839b316df9b1adf825de002179fb4bf
DINESH BAMNIA <mpkhadirajgarh@gmail.com>
2025-03-27 10:11:52 +0530

    done

---
+++ java script/Looping Statement/for.js
@@ -0,0 +1,27 @@
+for (let i = 1 ; i<=10; i++) {
+
+    console.log(`${i}:Aditya Computer Acadmey`);
+}
+
+function Table(Number) {
+
+    for( let i = 1; i<=10; i++) {
+
+        console.log(Number*i); 
+    }
+}
+
+Table(15);
+
+function Squre(n) {
+
+    console.log(n**2)
+}
+
+Squre(100);
+Squre(15);
+
+// square root 
+// Factorial 
+
+
