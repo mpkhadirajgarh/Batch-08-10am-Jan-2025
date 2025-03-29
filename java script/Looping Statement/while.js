4b4c061ab8eed854087c4843f8dbc606793720a8
DINESH BAMNIA <mpkhadirajgarh@gmail.com>
2025-03-29 10:16:04 +0530

    done

---
+++ java script/Looping Statement/while.js
@@ -0,0 +1,22 @@
+let i = 1 ;
+
+while(i<=10) {
+
+    console.log(`Number is:${i}`);
+    i++;
+}
+
+let a = ""
+
+for( let count=1 ; count<=11 ; count++) {
+
+    a += "My Number:" + count ; 
+}
+
+console.log(a);
+
+let b = 10 ;
+
+b += 20 // b = b + 20; 
+
+console.log(b);
