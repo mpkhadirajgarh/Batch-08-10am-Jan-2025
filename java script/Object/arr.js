07897fc60f1560a951134c944c462311f8bf93a3
DINESH BAMNIA <mpkhadirajgarh@gmail.com>
2025-04-08 10:12:41 +0530

    Add files via upload

---
+++ java script/Object/arr.js
@@ -0,0 +1,19 @@
+// Make a array
+
+const courseName = ["DCA","PGDCA","Tally","CPCT","BCC",985154]
+   // Arrary index   0       1       2       3     4
+
+
+
+// console.log(courseName[0]);
+
+for(let i = 0 ;i<courseName.length; i++ ){
+
+    console.log(courseName[i])
+}
+
+
+
+ 
+
+
---
+++ java script/Object/obj.js
@@ -0,0 +1,11 @@
+const StudentName = {
+
+    // key : value pairs
+
+    SName: "Rajkumar Mewada",
+    className : "PGDCA",
+    MobileNumber: 9854584587,
+}
+
+console.log(StudentName.SName); // Object value nikal li hai 
+
